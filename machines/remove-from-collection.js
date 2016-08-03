528adf4d181621fc6783f6fe1986cadd363b9095
Scott Gress <scottmgress@gmail.com>
2016-08-02 22:42:55 -0500

    Implement `removeFromCollection` and add test

--- machines/remove-from-collection.js
+++ machines/remove-from-collection.js
@@ -50,48 +50,81 @@ module.exports = {
   },
 
 
-  fn: function(inputs, exits) {
+  fn: function(inputs, exits, env) {
+
+    // Import `isObject` and `isUndefined` Lodash functions.
     var _isObject = require('lodash.isobject');
     var _isUndefined = require('lodash.isundefined');
 
+    // If we can't access the ORM, leave through the `error` exit.
     if (!_isObject(env.sails.hooks.orm)) {
       return exits.error(new Error('`sails.hooks.orm` cannot be accessed; please ensure this machine is being run in a compatible habitat.'));
     }
 
-    // Check to ensure `inputs.model` is a recognized model.
+    // Find the model class indicated by the `inputs.model` value.
     var Model = env.sails.hooks.orm.models[inputs.model];
+
+    // If it's not a recognized model, trigger the `error` exit.
     if (!_isObject(Model)) {
       return exits.error(new Error('Unrecognized model (`'+inputs.model+'`).  Please check your `api/models/` folder and check that a model with this identity exists.'));
     }
+
     // Check to ensure `inputs.association` is a recognized collection association
     if (!_isObject(Model.attributes[inputs.association])) {
       return exits.error(new Error('Unrecognized association (`'+inputs.association+'`).  Please check that a "collection" association named `'+inputs.association+'` is defined as an attribute of this model (`'+inputs.model+'`).'));
     }
+
+    // If the association is a model, not a collection, trigger the `error` exit.
     else if (!Model.attributes[inputs.association].collection) {
       return exits.error(new Error('Invalid association (`'+inputs.association+'`).  This method is only compatible with "collection" associations.'));
     }
 
-    // Start building query
-    var q = Model.removeFromCollection(inputs.recordId, inputs.association, inputs.associatedIdsToRemove);
-
-    // Use metadata if provided.
-    if (!_isUndefined(inputs.meta)) {
-      q = q.meta(inputs.meta);
-    }
-
-    // Use existing connection if one was provided.
-    if (!_isUndefined(inputs.connection)) {
-      q = q.usingConnection(inputs.connection);
-    }
-
-    // Execute query
-    q.exec(function afterwards(err) {
+    // Find the record we want to remove associated records from.
+    Model.findOne(inputs.recordId).exec(function afterFindOne(err, record) {
+      // Forward any errors to the `error` exit.
       if (err) {
-        // TODO: negotiate error in order to trigger `exits.recordNotFound()`
         return exits.error(err);
       }
-      return exits.success();
+      // If the record could not be found, trigger the `notFound` exit.
+      if (!record) {
+        return exits.recordNotFound();
+      }
+      // Remove the requested IDs from the specified collection.
+      record[inputs.association].remove(inputs.associatedIdsToRemove);
+      // Save the record.
+      record.save(function afterwards(err) {
+        // Forward any errors to the `error` exit.
+        if (err) {
+          return exits.error(err);
+        }
+        // Return via the `success` exit.
+        return exits.success();
+      });
     });
+
+    // // Start building the query.
+    // var q = Model.removeFromCollection(inputs.recordId, inputs.association, inputs.associatedIdsToRemove);
+
+    // // Use metadata if provided.
+    // if (!_isUndefined(inputs.meta)) {
+    //   q = q.meta(inputs.meta);
+    // }
+
+    // // Use existing connection if one was provided.
+    // if (!_isUndefined(inputs.connection)) {
+    //   q = q.usingConnection(inputs.connection);
+    // }
+
+    // // Execute the query.
+    // q.exec(function afterwards(err) {
+    //   // Forward any errors to the `error` exit.
+    //   if (err) {
+    //     // TODO: negotiate error in order to trigger `exits.recordNotFound()`
+    //     return exits.error(err);
+    //   }
+    //   // Return through the `success` exit.
+    //   return exits.success();
+    // });
   }
 
 
---
+++ tests/remove-from-collection.js
@@ -0,0 +1,90 @@
+var assert = require('assert');
+var path = require('path');
+var SailsApp = require('sails').Sails;
+var Waterline = require('../');
+var fsx = require('fs-extra');
+var _ = require('lodash');
+
+describe('machinepack-waterline: remove-from-collection', function() {
+
+  var Sails = new SailsApp();
+  var app;
+  before(function(done) {
+    process.chdir(path.resolve(__dirname, 'fixtures', 'app'));
+    fsx.copySync('localDiskDb.db', path.resolve('.tmp', 'localDiskDb.db'));
+    Sails.load({
+      hooks: {grunt: false, views: false},
+      globals: false
+    }, function(err, _sails) {
+      if (err) {return done(err);}
+      app = _sails;
+      return done();
+    });
+  });
+
+  after(function(done) {
+    app.lower(function(err) {
+      if (err) {return done(err);}
+      setTimeout(done, 500);
+    });
+  });
+
+  describe('when called with valid inputs', function() {
+
+    it('should remove the requested items from the collection', function(done) {
+
+      // Attempt to remove "farley" from "bob"
+      Waterline.removeFromCollection({
+        model: 'user',
+        recordId: 1,
+        association: 'pets',
+        associatedIdsToRemove: [1]
+      })
+      .setEnvironment({sails: app})
+      .exec({
+        error: done,
+        success: function() {
+
+          // See which pets "bob" has now
+          app.models.user.findOne(1).populate('pets').exec(function(err, bob) {
+            if (err) {return done(err);}
+            assert.equal(bob.pets.length, 1);
+            assert(_.find(bob.pets, {id: 2}));
+            return done();
+          });
+
+        }
+      });
+
+    });
+
+  });
+
+  describe('when called with a record ID that doesn\'t exist', function() {
+
+    it('should trigger the `recordNotFound` exit', function(done) {
+
+      // Attempt to add "roscoe" and "argus" to "joe"
+      Waterline.removeFromCollection({
+        model: 'user',
+        recordId: 99,
+        association: 'pets',
+        associatedIdsToRemove: [4,5]
+      })
+      .setEnvironment({sails: app})
+      .exec({
+        error: done,
+        success: function() {
+          return done(new Error('Should not have called `success` exit!'));
+        },
+        recordNotFound: done
+
+      });
+
+    });
+
+  });
+
+});
+
+
